dc933d80b2e60502d9b64320218b26802fb9a2c4
aaquib99 <memonaaquib@gmail.com>
2021-12-21 17:40:22 +0530

    final

--- Config/config.js
+++ Config/config.js
@@ -10,7 +10,7 @@ const config = {
         host: process.env.HEROKU_DB_HOST || '',
         port: 7869,
         //url: process.env.HEROKU_DB_URL || '',
-        ssl: { rejectUnauthorized: false },
+        //ssl: { rejectUnauthorized: false },
         //max: parseInt(process.env.DB_MAX_CLIENTS) || 20,
         //idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT_MS) || 30000
     }

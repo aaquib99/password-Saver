5281cf6379c6462ccefd80b29a475396dd36ef74
aaquib99 <memonaaquib@gmail.com>
2021-12-21 19:39:24 +0530

    final

--- Config/config.js
+++ Config/config.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const config = {
     //serviceName: process.env.SERVICENAME || 'PostgresDB',
-    port: process.env.PORT || 5432,
+    port: process.env.PORT || 5000,
     loggerLevel: process.env.LOGGERLEVEL || 'debug',
     db:{
         user: process.env.HEROKU_USER || '',
         database: process.env.HEROKU_DB || '',
         password: process.env.HEROKU_DB_PASS || '',
         host: process.env.HEROKU_DB_HOST || '',
-        port: 7869,
+        port: 5000,
         //url: process.env.HEROKU_DB_URL || '',
         ssl: { rejectUnauthorized: false },
         //max: parseInt(process.env.DB_MAX_CLIENTS) || 20,
